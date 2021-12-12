2432508e8e4b3b5b69199ce57db0e3d7c3deb041
Алексей <alexleskov2000@gmail.com>
2021-12-12 21:43:19 +0800

    Вход и выход пользователя

--- config/settings.py
+++ config/settings.py
@@ -100,3 +100,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'news:list'
+LOGOUT_REDIRECT_URL = 'news:list'
