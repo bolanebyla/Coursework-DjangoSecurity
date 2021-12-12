eb3ac5a2869575f52d7d6b6aa695c878acdd4436
Алексей <alexleskov2000@gmail.com>
2021-12-12 21:19:49 +0800

    Добавить автора

--- news/models.py
+++ news/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class News(models.Model):
     title = models.CharField('Название', max_length=150)
     text = models.TextField('Новость')
+
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
--- news/views.py
+++ news/views.py
@@ -9,11 +9,14 @@ class NewsListView(generic.ListView):
     model = models.News
 
 
-
 class NewsCreateView(LoginRequiredMixin, generic.CreateView):
     model = models.News
     fields = ('title', 'text',)
     template_name = 'attacks_demonstration/xss_form.html'
 
+    def form_valid(self, form):
+        super(NewsCreateView, self).form_valid(form)
+
     def get_success_url(self):
-        return reverse('news:list', kwargs={'pk': self.object.id})
+        return reverse('news:list')
+    
